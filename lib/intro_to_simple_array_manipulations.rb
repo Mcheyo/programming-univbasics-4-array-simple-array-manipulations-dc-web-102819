0a8aaca6326cdc9f232fad6f4ad6feeca6aa85a6
Mansour Cheyo <mcheyo.mc@gmail.com>
2019-10-15 22:52:00 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_push (artists, next_artists)
+artists = ["Hozier", "Ariana Grande", "Usher"]
+next_artist = "violet"
+artists.push(next_artist)
+end
+
+def using_unshift(artists, next_artist)
+  artists = ["Hozier", "Ariana Grande", "Usher", "Maya"]
+  next_artist = "Staten Island"
+  artists.unshift(next_artist)
+end
+
+def using_pop (continent)
+  continent  = ["Africa", "Asia", "NorthA", "SouthA", "Europe", "Australia", "Antarctica"]
+  ouch = continent.pop(ouch)
+  p ouch
+end
